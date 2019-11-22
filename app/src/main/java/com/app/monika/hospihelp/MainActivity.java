6b51820aaca574e8b7ab8ccf1be4d26f29500d29
monika <monikasharma1899@gmail.com>
2019-11-22 12:59:59 +0530

    Initial Commit- Changes in UI

--- app/src/main/java/com/app/monika/hospihelp/MainActivity.java
+++ app/src/main/java/com/app/monika/hospihelp/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button search_button;
     EditText search_bar;
+    ImageView home, one, two, profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +23,36 @@ public class MainActivity extends AppCompatActivity {
         search_button = (Button) findViewById(R.id.srch_btn);
         search_bar = findViewById(R.id.srch_bar);
 
+        home=(ImageView)findViewById(R.id.homebtn);
+        one=(ImageView)findViewById(R.id.btn1);
+        two=(ImageView)findViewById(R.id.btn2);
+        profile=(ImageView)findViewById(R.id.profilebtn);
+
         search_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, EmergencySearch.class));
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FirstActivity.class));
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
     }
